aa03bd698873b5a04e1c12b16a3c4d735a33e02a
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-08-24 22:07:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,12 +48,3 @@ def reduce_to_total(source_array, starting_point=0)
   return total
 end
 
-def reduce_to_total(source_array, starting_point)
-  total = starting_point
-  i = 0
-  while i < source_array.length do
-    total += source_array[i]
-    i += 1
-  end
-  return total
-end
