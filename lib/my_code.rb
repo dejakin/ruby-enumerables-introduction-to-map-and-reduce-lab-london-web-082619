0d4e40d68b71fa1a3df0a9e9a7167da3aa204322
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-08-24 22:21:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -51,11 +51,7 @@ end
 def reduce_to_all_true(source_array)
   i = 0
   while i < source_array.length do
-    if source_array[i]
-      return true
-    else
-      return false
-    end
+   return false if source_array[i] == false
     i += 1
   end
 end
