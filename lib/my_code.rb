b18e4a36dea176875ea1b276375f0f9cd10d8570
Deji Akinyemi <deji.akinyemi@hotmail.co.uk>
2019-08-24 22:29:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -60,8 +60,8 @@ end
 def reduce_to_any_true(source_array)
   i = 0
   while i < source_array.length do
-   return false if source_array[i] == false
+   return true if source_array[i] == true
     i += 1
   end
-  return true
+  return false
 end
